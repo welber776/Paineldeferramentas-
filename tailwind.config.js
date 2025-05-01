8da8bf318a72c32c59b9a7731b44da207436c8f4
welber776 <welberdossantos2@gmail.com>
2025-05-01 12:44:52 -0300

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        border: "#e5e7eb" // cor personalizada que você estava tentando usar
+      }
+    },
+  },
+  plugins: [],
+}
